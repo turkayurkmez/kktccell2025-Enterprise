c1a39a966afe506fdd90054bf9a6d5ee86515fa1
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-30 14:49:16 +0300

    Initialize .NET 9.0 solution with API and domain structure
    Added a new solution structure for a .NET 9.0 application with
    four projects: Catalog.API, Catalog.Application, Catalog.Entities,
    and Catalog.Infrastructure. Configured logging, OpenAPI, and HTTP
    testing for the API. Introduced a WeatherForecast API as a sample
    implementation.
    
    Included .gitignore and .gitattributes files to ensure proper
    version control practices. Added a generic Entity base class and
    a Category domain model to establish the foundation for domain
    modeling. Configured project references and build settings.

---
+++ Catalog/Catalog.Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Entities
+{
+    public class Category: Entity<int>
+    {       
+        public string Name { get; private set; }
+    }
+}
---
+++ Catalog/Catalog.Entities/Common/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Entities.Common
+{
+    public abstract class Entity<T> where T: struct, IEquatable<T> 
+    {
+        public T Id { get; protected set; }
+        public DateTime CreatedDate { get; protected set; }
+        public DateTime? UpdatedDate { get; protected set; }
+
+        protected Entity()
+        {
+            CreatedDate = DateTime.Now;
+            Id = typeof(T) == typeof(Guid) ?(T)(object) Guid.NewGuid() : default!;
+        }
+
+
+
+
+    }
+}
